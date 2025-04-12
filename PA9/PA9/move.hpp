f087de809abe677e52a6c481d2ce721b9d8f2a36
Christian Gonsalves <ChristianAGonsalves@gmail.com>
2025-04-11 22:36:24 -0700

    Readded classes from previous repository

---
+++ PA9/PA9/move.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+
+class Moves {
+public:
+
+
+private:
+    std::string moveName;
+    double power,	 // Multiplier
+           speed,
+           accuracy;
+    int moveCount; // Max # of times a move can be used
+    char moveType; 
+
+    // some sort of status eefect moodifier
+    // an array to hold status effect things;
+};
