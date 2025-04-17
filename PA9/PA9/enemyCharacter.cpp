39588a6d1f788d0e475eb8965869f9d5a0a0607c
G-Sico <mypeople2626@gmail.com>
2025-04-17 11:29:54 -0700

    added readfromfile

--- PA9/PA9/enemyCharacter.cpp
+++ PA9/PA9/enemyCharacter.cpp
@@ -13,9 +13,26 @@ string EnemyCharacter::getCatchPhrase(void) const
 void EnemyCharacter::readFromFile(EnemyCharacter& n, ifstream& instream)
 {
 	string tempName = "", tempAtt = "", tempMxHp = "", tempCurrHp = "", tempAgil = "", tempAcc = "", tempDef = "",
-		tempMove = "";
+		tempPharse = "", tempMove = "";
 
 	getline(instream, tempName, ',');
+	getline(instream, tempAtt, ',');
+	getline(instream, tempMxHp, ',');
+	getline(instream, tempCurrHp, ',');
+	getline(instream, tempAgil, ',');
+	getline(instream, tempAcc, ',');
+	getline(instream, tempDef, ',');
+	getline(instream, tempPharse, ',');
+
+	n.setName(tempName);
+	n.setAttack(stoi(tempAtt));
+	n.setMaxHealth(stoi(tempMxHp));
+	n.setCurrentHealth(stoi(tempCurrHp));
+	n.setAgility(stoi(tempAgil));
+	n.setAccuracy(stoi(tempAcc));
+	n.setDefense(stoi(tempDef));
+	n.setCatchPhrase(tempPharse);
+
 
 
 }
