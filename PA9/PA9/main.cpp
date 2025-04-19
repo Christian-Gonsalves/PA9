3b30a78af60f8d5219d5c8513568c6a526459f17
Andres <drevcardenas27@gmail.com>
2025-04-18 17:33:50 -0700

    Added sound and a window

--- PA9/PA9/main.cpp
+++ PA9/PA9/main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
 
 int main() {
 
-	return 0;
+	sf::RenderWindow window(sf::VideoMode({ 1200, 1200 }),"TA BASH"); 
+	sf::Music music;
+	if (!music.openFromFile("../Audio/sound1.wav"))
+		return -1; // error
+
+	music.play();
+
+
+	while (window.isOpen())
+	{
+
+		while (const std::optional event = window.pollEvent())
+		{
+
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close(); 
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+			{
+
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+			{
+
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))\
+			{
+
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+			{
+
+
+			}
+
+			window.clear(); 
+			window.display(); 
+		}
+	}
 }
