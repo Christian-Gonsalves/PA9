39878d3f291c23be53d2eabbf18c3d4b333f5798
Andres <drevcardenas27@gmail.com>
2025-04-19 14:45:46 -0700

    Merging changes

--- PA9/PA9/character.cpp
+++ PA9/PA9/character.cpp
@@ -54,7 +54,8 @@ void Character::setDefense(const int newDefense)
 
 void Character::setStatusEffects(const int& newStatusEffect)
 {
-	*statusEffects = newStatusEffect;
+	this->statusEffects[index] = turns; 
+	this->statusEffects[index + 1] = strength; 
 }
 
 void Character::setMoveSet(const Moves& newMoveSet)
@@ -97,9 +98,14 @@ int Character::getDefense(void) const
 	return defense;
 }
 
+int Character::getStatusEffectTurns(int index)	//put in the index for the start of attribute
+{
+	return this->statusEffects[index];
+}
+
 int& Character::getStatusEffect(void)
 {
-	return *this->statusEffects;
+	return this->statusEffects;
 }
 
 Moves& Character::getMoveSet(void)
--- PA9/PA9/main.cpp
+++ PA9/PA9/main.cpp
@@ -1,52 +1,11 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
+#include "Screen.hpp"
 
 
 int main() {
+	sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "SFML window");
+	Screen s;
 
-	sf::RenderWindow window(sf::VideoMode({ 1200, 1200 }),"TA BASH"); 
-	//sf::Music music;
-	//if (!music.openFromFile("../Audio/sound1.wav"))
-		//return -1; // error
-
-	//music.play();
-
-
-	while (window.isOpen())
-	{
-
-		while (const std::optional event = window.pollEvent())
-		{
-
-			if (event->is<sf::Event::Closed>())
-			{
-				window.close(); 
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-			{
-
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-			{
-
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))\
-			{
-
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-			{
-
-
-			}
-
-			window.clear(); 
-			window.display(); 
-		}
-	}
+	s.run(window);
+	return 0;
 }
