2103b606503833f0ebf69c088d428e69c6207f02
Christian Gonsalves <ChristianAGonsalves@gmail.com>
2025-04-19 13:33:31 -0700

    Deleted .vs Folder & unfinished turn wrapper

--- PA9/PA9/move.hpp
+++ PA9/PA9/move.hpp
@@ -11,7 +11,8 @@ private:
     double power,	 // Multiplier
            speed,
            accuracy;
-    int moveCount; // Max # of times a move can be used
+    int maxMoveCount, // # of times a move can be used
+        currentMoveCount;
     char moveType; 
 
     // some sort of status eefect moodifier
---
+++ PA9/PA9/turnWrapper.cpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "turnWrapper.hpp"
+
+bool TurnWrapper::runBattle() {
+	while (isBattleOver() == false) {
+
+
+	}
+}
+
+bool TurnWrapper::isBattleOver() {
+	if (enemy->getCurrentHealth() > 0 && player->getCurrentHealth()) {
+		return true;
+	}
+		return false;
+}
