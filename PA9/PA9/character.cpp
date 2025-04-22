7e0456e0627572a01d27244142e3ff0c3baaacc5
Christian Gonsalves <ChristianAGonsalves@gmail.com>
2025-04-21 19:01:18 -0700

    AAAAAAAAA

--- PA9/PA9/character.cpp
+++ PA9/PA9/character.cpp
@@ -74,6 +74,14 @@ void Character::setMoveSet(const Move* newMoveSet)
 	}
 }
 
+void Character::setLastTypeUsed(char newLastTypeUsed) {
+
+}
+
+char Character::getLastTypeUsed(void) const {
+	
+}
+
 string Character::getName(void) const
 {
 	return name;
--- PA9/PA9/character.hpp
+++ PA9/PA9/character.hpp
@@ -6,7 +6,7 @@
 class Character {
 public:
 	Character(string newName  = "", int newAttack = 0, int newMaxHealth = 0, int newCurrentHealth = 0, int newAgility = 0, int newAccuracy = 0,
-		int newDefense = 0, int* newStatusEffects = nullptr);
+		int newDefense = 0, int* newStatusEffects = nullptr, char newLastTypeUsed);
 	~Character();
 
 	void setName(const string newName);
@@ -20,6 +20,7 @@ public:
 	//index is where the turns will be held, index+1 is strength, [0] is attack turns, [1] is attack modifier
 	// ex index = 0, strength = .25, turns = 1, ==> [0] = 1. [1] = .25
 	void setMoveSet(const Move* newMoveSet);
+	void setLastTypeUsed(char newLastTypeUsed);
 
 	string getName(void) const;
 	int getAttack(void) const;
@@ -31,7 +32,7 @@ public:
 	int getStatusEffectTurns(int index);
 	int getStatusEffectStrength(int index);
 	Move& getMoveSet(void);
-
+	char getLastTypeUsed(void) const;
 
 private:
 	string name;
@@ -44,5 +45,6 @@ private:
     int statusEffects [10];	//an array for checking which status effects character has. Each status effect has 2 elements. First if for turn duration, second is intensity. 0: str; 2: def; 4: spd; 6: eva; 8: stun; 
 	Move moveSet[12];	//an array of moves that the charcater has access to
 	// the chracter model
+	char lastTypeUsed; // type of last move used 
 };
 
