5a987d86f547b2b26bcddc76e5333845843b1b35
AlexL-32 <alejandro.n.lopez@wsu.edu>
2025-04-21 20:40:39 -0700

    added dialog in battleScreen

--- PA9/PA9/battleScreen.cpp
+++ PA9/PA9/battleScreen.cpp
@@ -1,8 +1,15 @@
 #include "BattleScreen.hpp"
 
-BattleScreen::BattleScreen(sf::Texture& bgTex, sf::Texture& andyTex) : exitBattle(false), bgTex(bgTex), bg(bgTex), andy(andyTex), andyTexture(andyTex)
+BattleScreen::BattleScreen(sf::Texture& bgTex, sf::Texture& andyTex, sf::Font& font) : exitBattle(false), showDialog(true), bgTex(bgTex), bg(bgTex), andy(andyTex), andyTexture(andyTex), dialog(font)
 {
-
+    dialogBox.setSize(sf::Vector2f(350.f, 200.f));
+    dialogBox.setFillColor(sf::Color::White);
+    dialogBox.setPosition(sf::Vector2f(400.f, 300.f));
+    dialog.setFont(font);
+    dialog.setString("Andy: \n\nhello, world!");
+    dialog.setCharacterSize(24);
+    dialog.setFillColor(sf::Color::Black);
+    dialog.setPosition(sf::Vector2f(420.f, 320.f));
 }
 
 
@@ -12,6 +19,10 @@ void BattleScreen::handleInput(sf::RenderWindow& window)
     {
         exitBattle = true;
         sf::sleep(sf::milliseconds(200));
+    }
+    if (showDialog && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter))
+    {
+        showDialog = false;
     }
 }
 
@@ -25,6 +36,14 @@ void BattleScreen::draw(sf::RenderWindow& window)
 {
     window.draw(bg);
 	andy.draw(window);
+    if (andy.hasEnteredBattle())
+    {
+        if (showDialog)
+        {
+            window.draw(dialogBox);
+            window.draw(dialog);
+        }
+    }
 }
 
 bool BattleScreen::shouldExitBattle() const
