e82b6cceeb633ccdc1e6d556522cf303a3bdc057
G-Sico <mypeople2626@gmail.com>
2025-04-25 22:36:16 -0700

    fixed audio?

--- PA9/PA9/gameMange.cpp
+++ PA9/PA9/gameMange.cpp
@@ -17,7 +17,7 @@ GameManage::GameManage()
     levelScreen = std::make_unique<LevelSelectScreen>(playerTex, lvlBgTex);
     battleScreen = std::make_unique<BattleScreen>(battleBgTex, andyTex, inBattlePlayerTex, font);
 
-    this->buffer = new sf::SoundBuffer[3]{ sf::SoundBuffer("assets/Audio/sound1.wav"), sf::SoundBuffer("assets/Audio/sound2.wav"), sf::SoundBuffer("assets/Audio/sound5.ogg") };
+    this->buffer = new sf::SoundBuffer[3]{ sf::SoundBuffer("assets/Audio/sound1_2.wav"), sf::SoundBuffer("assets/Audio/sound2.wav"), sf::SoundBuffer("assets/Audio/sound5.ogg") };
     this->sound = new sf::Sound[3]{ sf::Sound(buffer[0]), sf::Sound(buffer[1]), sf::Sound(buffer[2]) };
 
     curScreen = levelScreen.get();
