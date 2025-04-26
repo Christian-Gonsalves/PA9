71b649cdf3591f6c384f50345a9760adddea1f1a
Andres Valencia Cardenas <98302359+AndyCaliWASH@users.noreply.github.com>
2025-04-25 18:50:52 -0700

    Update gameMange.cpp
    Added sound

--- PA9/PA9/gameMange.cpp
+++ PA9/PA9/gameMange.cpp
@@ -16,6 +16,9 @@ GameManage::GameManage()
     levelScreen = std::make_unique<LevelSelectScreen>(playerTex, lvlBgTex);
     battleScreen = std::make_unique<BattleScreen>(battleBgTex, andyTex, inBattlePlayerTex, font);
 
+    this->buffer = new sf::SoundBuffer[3]{ sf::SoundBuffer("Audio/sound1.wav"), sf::SoundBuffer("Audio/sound2.wav"), sf::SoundBuffer("Audio/sound5.ogg") };
+    this->sound = new sf::Sound[3]{ sf::Sound(buffer[0]), sf::Sound(buffer[1]), sf::Sound(buffer[2]) };
+
     curScreen = levelScreen.get();
 }
     
@@ -26,8 +29,16 @@ void GameManage::run()
     EnemyCharacter enemy;
     bool winState;
 
+    Menu menu(this->window.getSize().x, this->window.getSize().y);
+    menu.load_menu(this->window);
+    sound[0].play();
+
     player.readFromFile("Player_Character.csv");
     enemy.readFromFile("Andy_Character.csv");
+
+    Menu menu(this->window.getSize().x, this->window.getSize().y);
+    menu.load_menu(this->window);
+    sound[0].play();
 
     player.sortMoves();
 
@@ -53,6 +64,12 @@ void GameManage::run()
 
         // check screen transitions
         if (levelScreen->shouldStartBattle()) {
+            if (sound[0].getStatus() == sf::Sound::Status::Playing)
+            {
+                sound[0].stop();
+            }
+            sound[1].setLooping(true);
+            sound[1].play();
 
             // Beginning battle Animation
             while (!battleScreen->hasAnimationConcluded()) {
@@ -64,12 +81,22 @@ void GameManage::run()
             }
 
             winState = mainBattle.runBattle();
-            
+
             // Temporary mesure. Should set up some way of deciding what player & enemy should be besides the defaultssss
             mainBattle.setPlayer(player);
             mainBattle.setEnemy(enemy);
 
             if (winState) {
+
+                sound[2].play();
+                if (sound[1].getStatus() == sf::Sound::Status::Playing)
+                {
+                    sound[1].stop();
+                }
+                sound[0].setLooping(true);
+                sound[0].play();
+
+
                 window.close();
             }
         }
